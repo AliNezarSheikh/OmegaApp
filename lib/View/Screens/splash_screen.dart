66d84f40b616b1c8b9c2c27e1046461a815e6cba
AliNezarSheikh <alinsheikh2020@gmail.com>
2024-02-05 22:20:18 +0400

    remember

--- lib/View/Screens/splash_screen.dart
+++ lib/View/Screens/splash_screen.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:omega/Constant/Components.dart';
-import 'package:omega/View/Screens/home_screen.dart';
 
+import 'home_screen.dart';
 import 'login_screen.dart';
 
+
 class splashscreen extends StatelessWidget {
-  const splashscreen({super.key});
+   splashscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     Future.delayed(Duration(seconds: 4),(){
       remeber.read("token")==null ?
-     Get.off(()  =>loginscreen(),transition: Transition.circularReveal,curve: Curves.easeInOut,duration: Duration(seconds: 3))
+      Get.off(()  =>loginscreen(),transition: Transition.circularReveal,curve: Curves.easeInOut,duration: Duration(seconds: 3))
           :Get.off(()  =>homescreen(),transition: Transition.circularReveal,curve: Curves.easeInOut,duration: Duration(seconds: 3));
     });
     return  SafeArea(
--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
 
       debugShowCheckedModeBanner: false,
-      home: loginscreen(),
+      home: splashscreen(),
 
       initialBinding: BindingsBuilder(() {
         Get.lazyPut<homecontroller>(() => homecontroller());
